f1a562676044621e5a4e14f5866afbf98a856915
Leonardo <20171484@aloe.ulima.edu.pe>
2021-09-18 15:15:22 -0500

    Angular material+ carpeta component
    Hice un push, en resumen, todos trabajaremos con la carpeta components, y ni Diego ni yo estamos seguros de qué pasaría si creamos una misma carpeta con un mismo nombre y hacemos 2 pushes distintos, voy a hacer un push con la carpeta ya creada para que todos creen sus propios componentes dentro de esa carpeta, he borrado los cambios que había hecho del proyecto original lo cual solo eran elementos sueltos que usaba para pruebas mientras practicaba con angular así que es prácticamente una versión limpia pero con la carpeta componentes creada, pueden crear sus componentes escribiendo en la terminal: ng c components components/nombredetucomponente. También agregué la dependencia de angular material, la cual se corría con: ng add @angular/material, así que ya no sería necesario que lo corran de nuevo al pullear esta versión.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import { MatSliderModule } from '@angular/material/slider';
+//Componentes
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
