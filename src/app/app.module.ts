5b235492545c07af9a5d3796ccd85b49e480d3c5
Leonardo <20171484@aloe.ulima.edu.pe>
2021-09-18 15:34:13 -0500

    Carpeta components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
 
-//Angular Material
-import { MatSliderModule } from '@angular/material/slider';
-//Componentes
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+
 
   ],
   providers: [],
