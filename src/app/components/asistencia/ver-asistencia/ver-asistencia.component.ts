29a85d4ec6c6228a4e588f24ba80c405b22221e6
espadianto2000 <20170532@aloe.ulima.edu.pe>
2021-11-07 15:14:37 -0500

    asistencia terminado, sin revisar elementos visuales

--- src/app/components/asistencia/ver-asistencia/ver-asistencia.component.ts
+++ src/app/components/asistencia/ver-asistencia/ver-asistencia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { of } from 'rxjs';
 import { AsistenciaGeneral } from 'src/app/entities/asistenciaGeneral';
 import { AsistenciaComponent } from '../asistencia.component';
 import { AsistenciaService } from '../asistencia.service';
@@ -11,6 +12,7 @@ import { AsistenciaService } from '../asistencia.service';
   styleUrls: ['./ver-asistencia.component.css']
 })
 export class VerAsistenciaComponent implements OnInit {
+  myId: string = sessionStorage.getItem('id')!;
   asistenciaId: number = 0;
   asistencia:AsistenciaGeneral|undefined;
   constructor(private api:AsistenciaService, public router:Router, private dialogRef:MatDialogRef<AsistenciaComponent>) { }
@@ -19,6 +21,19 @@ export class VerAsistenciaComponent implements OnInit {
     this.conseguirAsistencia();
   }
   conseguirAsistencia(){
-    
+    var encontrado:boolean=false;
+    for(var i = 0; i < this.asistencia!.userAttendances!.length; i++){
+      if(this.asistencia!.userAttendances![i].user.id+""==this.myId){
+        encontrado = true
+      }
+    }
+    console.log(encontrado);
+    if(encontrado==false){
+      var h1El = document.createElement("h3");
+      const contenido = document.createTextNode("No apareces en el registro de esta clase"); 
+      h1El.appendChild(contenido);
+      //document.body.insertBefore(h1El,);
+      document.getElementById('lista')!.appendChild(h1El);
+    }
   }
 }
