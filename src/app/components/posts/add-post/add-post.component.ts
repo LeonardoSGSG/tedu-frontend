8c3c3a7891554f2386e20b2804f602bedbfd8ff4
espadianto2000 <20170532@aloe.ulima.edu.pe>
2021-11-27 00:23:40 -0500

    Funcionalidad de envio de archivos en publicacion terminada

--- src/app/components/posts/add-post/add-post.component.ts
+++ src/app/components/posts/add-post/add-post.component.ts
@@ -1,8 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Post } from 'src/app/entities/post';
+import { StorageService } from '../../storage/storage.service';
 import { PostsComponent } from '../posts.component';
 import { PostsService } from '../posts.service';
 
@@ -15,11 +17,14 @@ export class AddPostComponent implements OnInit {
   addPostForm = new FormGroup({
     text: new FormControl('',[Validators.required])
   })
+  archivos: any[]=[];
   nombresArchivos: string[]=[];
+  numeroArchivos: number=0;
 
   constructor(private api:PostsService,
               public router:Router,
-              private dialogRef:MatDialogRef<PostsComponent>) { }
+              private dialogRef:MatDialogRef<PostsComponent>,
+              private apiFiles:StorageService) { }
 
   ngOnInit(): void {
   }
@@ -27,11 +32,28 @@ export class AddPostComponent implements OnInit {
     this.api.createPost(form, sessionStorage.getItem('currentCourse')!).subscribe(
       res=>{
         console.log("se envió el formulario satisfactoriamente");
+        if(this.numeroArchivos>0){
+          for(let i=0; i<this.numeroArchivos;i++){
+            this.apiFiles.subirImagen(this.nombresArchivos[i]+"_"+Date.now(),this.archivos[i]).then(urlImagen=>{
+              console.log(urlImagen);
+              this.apiFiles.createPostFile(urlImagen!,res.id).subscribe(res=>{
+                console.log(res.id+" "+res.key);
+                console.log("Se guardo la url de firebase en la BD")
+              }),
+              (error: HttpErrorResponse)=>{
+                alert(error.message);
+              }
+            })
+          }
+          console.log("se envian los archivos a firestore y se retornan las urls")
+        }
       },
       err=>{
         console.log(err);
       }
     )
+    
+    
   }
   public agregar(){
     location.reload
@@ -40,17 +62,23 @@ export class AddPostComponent implements OnInit {
   cargarImagen(event:any){
     this.limpiarArchivos();
     let archivo = event.target.files;
-    console.log(event.target.files);
-    let reader=new FileReader();
+    this.numeroArchivos=event.target.files.length;
     for(let i=0; i<event.target.files.length;i++){
+      let reader=new FileReader();
       this.nombresArchivos.push(archivo[i].name);
+      reader.readAsDataURL(archivo[i]);
+      reader.onloadend=()=>{
+        console.log(reader.result);
+        this.archivos.push(reader.result);
+      }
     }
     
     //reader.
   }
   limpiarArchivos(){
     var e = document.getElementById("nombresArchivos");
-        
+    this.archivos=[];
+    this.nombresArchivos=[];
         //e.firstElementChild can be used.
         var child = e!.lastElementChild; 
         while (child) {
--- src/app/components/storage/storage.service.ts
+++ src/app/components/storage/storage.service.ts
@@ -1,6 +1,9 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import firebase from 'firebase/compat';
+import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/entities/post';
 import { environment } from 'src/environments/environment';
 
 firebase.initializeApp(environment.firebaseConfig)
@@ -10,6 +13,30 @@ firebase.initializeApp(environment.firebaseConfig)
 })
 export class StorageService {
   storageRef = firebase.app().storage().ref();
+  private apiServerUrl = environment.apiBaseUrl;
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+  async subirImagen(nombre:string, imgBase64:any){
+
+    try{
+      let response = await this.storageRef.child("files/"+nombre).putString(imgBase64,'data_url');
+      return await response.ref.getDownloadURL();
+      
+    }catch(err){
+      console.log(err);
+      return null;
+    }
+  }
+  public createPostFile(url: string, postId:number):Observable<{post:Post, key:string, id:number}>{
+    const opts={
+      headers: new HttpHeaders({
+     'Authorization': 'Token ' + sessionStorage.getItem('token')
+
+    })
+   };
+   let envio={key:url};
+   return this.http.post<{post:Post, key:string, id:number}>(this.apiServerUrl+"/courses/"+sessionStorage.getItem('currentCourse')+"/posts/"+postId+"/files",envio,opts);
+  }
 }
