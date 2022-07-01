f02018156f4059cb8d38537b2d71554d3953ff1d
espadianto2000 <20170532@aloe.ulima.edu.pe>
2022-07-01 17:44:36 -0500

    fixes login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { FormularioLogin } from './DTOS/FormularioLogin';
 import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -19,11 +28,12 @@ export class LoginComponent implements OnInit {
       password: ''
     }
     constructor(private loginService: LoginService, private router: Router, private snackBar: MatSnackBar) {
-      
-      
-
     }
-    
+    logForm = new FormGroup({
+      emailFormControl : new FormControl('', [Validators.required, Validators.email]),
+      passwordFormControl : new FormControl('', [Validators.required]),
+    });
+    matcher = new MyErrorStateMatcher();
 
   ngOnInit(): void {
   }
@@ -34,29 +44,40 @@ submitLogin()
 }
  public ingresar(): void
   {
-   this.loginService.Login(this.formularioLogin).subscribe(
-    res => {
-      var token:string = res.user.token;
-      var id:string= res.user.id.toString();
-      sessionStorage.setItem('id', id);
-      sessionStorage.setItem('token', token);
-      console.log("token: " + res.user.token);
-      this.router.navigate(['/courses']);
-      
-    },
-    err => {
-      if(err.status==404)
-      {
-        this.openSnackBar('Usuario no registrado','Aceptar');
+    if((<HTMLInputElement>(document.getElementById('inputCorreo'))).validity.typeMismatch){
+      this.openSnackBar('Formato de correo incorrecto','Aceptar');
+    }
+    else if((<HTMLInputElement>(document.getElementById('passwordInput'))).validity.tooShort){
+      this.openSnackBar('Contraseña incorrecta','Aceptar');
+    }
+    else{
+      this.loginService.Login(this.formularioLogin).subscribe(
+        res => {
+          var token:string = res.user.token;
+          var id:string= res.user.id.toString();
+          sessionStorage.setItem('id', id);
+          sessionStorage.setItem('token', token);
+          console.log("token: " + res.user.token);
+          this.router.navigate(['/courses']);
+          
+        },
+        err => {
+          if(err.status==404)
+          {
+            this.openSnackBar('Usuario no registrado','Aceptar');
+          }
+          if(err.status==401)
+          {
+            this.openSnackBar('Contraseña incorrecta', 'Aceptar');
+          }
+          if(err.status==400)
+          {
+            this.openSnackBar('Usuario no registrado', 'Aceptar');
+          }
       }
-      if(err.status==401)
-      {
-        this.openSnackBar('Credenciales inválidas', 'Aceptar');
-      }
-      
-      console.log(err);
-  }
-  )
+      );
+    }
+   
 
   }
   public registrar(): void{
@@ -65,7 +86,7 @@ submitLogin()
   }
   openSnackBar(message: string, action: string)
   {
-    this.snackBar.open(message, action);
+    this.snackBar.open(message, action,{duration:3000});
   }
   
 }
